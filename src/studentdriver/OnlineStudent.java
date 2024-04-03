fb56c4dcd053c0522c6c1f6fc3820e3df051f64a
AbbyRianne <abbyriannem@gmail.com>
2024-04-03 11:44:07 -0500

    4/3 In Class Work
    Created all classes

---
+++ src/studentdriver/OnlineStudent.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentdriver;
+
+/**
+ *
+ * @author S552370
+ */
+public class OnlineStudent extends StudentFees {
+    
+}
---
+++ src/studentdriver/StudentFees.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentdriver;
+
+/**
+ *
+ * @author S552370
+ */
+public class StudentFees {
+    private String studentName;
+    private int studentID;
+    private boolean isEnrolled;
+    private int CREDITS_PER_COURSE = 3;
+    private double PER_CREDIT_FEE = 543.50;
+    
+    public StudentFees(String studentName, int studentID, boolean isEnrolled){
+        
+    }
+    
+    public String getStudentName(){
+        
+    }
+    
+    public int getStudentID(){
+        
+    }
+    
+    public boolean isIsEnrolled(){
+        
+    }
+    
+    public int getCREDITS_PER_COURSE(){
+        return CREDITS_PER_COURSE;
+    }
+    
+    public double getPER_CREDIT_FEE(){
+        return PER_CREDIT_FEE;
+    }
+    
+    public void setStudentName(String studentName){
+        
+    }
+    
+    public void setStudentID(int studentID){
+        
+    }
+    
+    public void setIsEnrolled(boolean isEnrolled){
+        
+    }
+    
+    public double getPayableAmount(){
+        
+    }
+    
+    public String toString(){
+        
+    }
+}
